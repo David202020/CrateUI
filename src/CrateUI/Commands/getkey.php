87434fc87fa347757be728adcb775ed1df1f75af
xXNiceAssasinl0 <xXNiceAssasinl0@users.noreply.github.com>
2017-12-13 15:11:28 -0500

    Added Prefix For Key Command

--- src/CrateUI/Commands/getkey.php
+++ src/CrateUI/Commands/getkey.php
@@ -2,17 +2,24 @@
 
 namespace CrateUI\Commands;
 
-use CrateUI\Main;
+use pocketmine\Server;
+use pocketmine\Player;
+
 use pocketmine\command\CommandSender;
 use pocketmine\command\ConsoleCommandSender;
 use pocketmine\command\PluginCommand;
-use pocketmine\Player;
+
 use pocketmine\item\Item;
+
 use pocketmine\inventory\Inventory;
+
 use pocketmine\item\enchantment\Enchantment;
+
 use pocketmine\level\Level;
-use pocketmine\Server;
+
 use pocketmine\utils\TextFormat as C;
+
+use CrateUI\Main;
 
 class getkey extends PluginCommand{
 
@@ -26,6 +33,7 @@ class getkey extends PluginCommand{
     public function execute(CommandSender $sender, string $commandLabel, array $args): bool{
 
         #$inv = $player->getInventory();
+        $prefix = $this->getPlugin()->cfg->get("Prefix");
         $commonname = Item::get(131,1,1);
         $votename = Item::get(131,2,1);
         $rarename = Item::get(131,3,1);
@@ -48,7 +56,7 @@ class getkey extends PluginCommand{
             case "common":
             case "Common":
                     if (!$sender->hasPermission("crate.key")) {
-                        $sender->sendMessage("§cYou are not allow to do that.");
+                        $sender->sendMessage($prefix . " §cYou are not allow to do that.");
                         return false;
                     }
                     if (count($args) < 2) {
@@ -61,24 +69,24 @@ class getkey extends PluginCommand{
                         $player = $this->getPlugin()->getServer()->getPlayer($args[1]);
                     if (!$player instanceof Player) {
                         if ($player instanceof ConsoleCommandSender) {
-                            $sender->sendMessage("§cPlease enter a player name.");
+                            $sender->sendMessage($prefix . " §cPlease enter a player name.");
                             return false;
                         }
-                        $sender->sendMessage("§cThat Player doesnt exit.");
+                        $sender->sendMessage($prefix . " §cThat Player doesnt exit.");
                         return false;
                     }
             $e->setLevel(-1);
             $commonname->addEnchantment($e);
             $commonname->setCustomName("§aCommon");
             $player->getInventory()->addItem($commonname);
-            $player->sendMessage("§eYou receive a §aCommon §eKey.");
-            $sender->sendMessage("§9" . $player->getName() . " §ehas received a §aCommon §eKey.");
+            $player->sendMessage($prefix . " §eYou receive a §aCommon §eKey.");
+            $sender->sendMessage($prefix . " §9" . $player->getName() . " §ehas received a §aCommon §eKey.");
             break;
             case "2":
             case "vote":
             case "Vote":
                     if (!$sender->hasPermission("crate.key")) {
-                        $sender->sendMessage("§cYou are not allow to do that.");
+                        $sender->sendMessage($prefix . " §cYou are not allow to do that.");
                         return false;
                     }
                     if (count($args) < 2) {
@@ -91,24 +99,24 @@ class getkey extends PluginCommand{
                         $player = $this->getPlugin()->getServer()->getPlayer($args[1]);
                     if (!$player instanceof Player) {
                         if ($player instanceof ConsoleCommandSender) {
-                            $sender->sendMessage("§cPlease enter a player name.");
+                            $sender->sendMessage($prefix . " §cPlease enter a player name.");
                             return false;
                         }
-                        $sender->sendMessage("§cThat Player doesnt exit.");
+                        $sender->sendMessage($prefix . " §cThat Player doesnt exit.");
                         return false;
                     }
             $e->setLevel(-1);
             $votename->addEnchantment($e);
             $votename->setCustomName("§cVote");
             $player->getInventory()->addItem($votename);
-            $player->sendMessage("§eYou receive a §cVote §eKey.");
-            $sender->sendMessage("§9" . $player->getName() . " §ehas received a §cVote §eKey.");
+            $player->sendMessage($prefix . " §eYou receive a §cVote §eKey.");
+            $sender->sendMessage($prefix . " §9" . $player->getName() . " §ehas received a §cVote §eKey.");
             break;
             case "3":
             case "rare":
             case "Rare":
                     if (!$sender->hasPermission("crate.key")) {
-                        $sender->sendMessage("§cYou are not allow to do that.");
+                        $sender->sendMessage($prefix . " §cYou are not allow to do that.");
                         return false;
                     }
                     if (count($args) < 2) {
@@ -121,24 +129,24 @@ class getkey extends PluginCommand{
                         $player = $this->getPlugin()->getServer()->getPlayer($args[1]);
                     if (!$player instanceof Player) {
                         if ($player instanceof ConsoleCommandSender) {
-                            $sender->sendMessage("§cPlease enter a player name.");
+                            $sender->sendMessage($prefix . " §cPlease enter a player name.");
                             return false;
                         }
-                        $sender->sendMessage("§cThat Player doesnt exit.");
+                        $sender->sendMessage($prefix . " §cThat Player doesnt exit.");
                         return false;
                     }
             $e->setLevel(-1);
             $rarename->addEnchantment($e);
             $rarename->setCustomName("§6Rare");
             $player->getInventory()->addItem($rarename);
-            $player->sendMessage("§eYou receive a §6Rare §eKey.");
-            $sender->sendMessage("§9" . $player->getName() . " §ehas received a §6Rare §eKey.");
+            $player->sendMessage($prefix . " §eYou receive a §6Rare §eKey.");
+            $sender->sendMessage($prefix . " §9" . $player->getName() . " §ehas received a §6Rare §eKey.");
             break;
             case "4":
             case "mythic":
             case "Mythic":
                     if (!$sender->hasPermission("crate.key")) {
-                        $sender->sendMessage("§cYou are not allow to do that.");
+                        $sender->sendMessage($prefix . " §cYou are not allow to do that.");
                         return false;
                     }
                     if (count($args) < 2) {
@@ -151,24 +159,24 @@ class getkey extends PluginCommand{
                         $player = $this->getPlugin()->getServer()->getPlayer($args[1]);
                     if (!$player instanceof Player) {
                         if ($player instanceof ConsoleCommandSender) {
-                            $sender->sendMessage("§cPlease enter a player name.");
+                            $sender->sendMessage($prefix . " §cPlease enter a player name.");
                             return false;
                         }
-                        $sender->sendMessage("§cThat Player doesnt exit.");
+                        $sender->sendMessage($prefix . " §cThat Player doesnt exit.");
                         return false;
                     }
             $e->setLevel(-1);
             $mythicname->addEnchantment($e);
             $mythicname->setCustomName("§5Mythic");
             $player->getInventory()->addItem($mythicname);
-            $player->sendMessage("§eYou receive a §5Mythic §eKey.");
-            $sender->sendMessage("§9" . $player->getName() . " §ehas received a §5Mythic §eKey.");
+            $player->sendMessage($prefix . " §eYou receive a §5Mythic §eKey.");
+            $sender->sendMessage($prefix . " §9" . $player->getName() . " §ehas received a §5Mythic §eKey.");
             break;
             case "5":
             case "legendary":
             case "Legendary":
                     if (!$sender->hasPermission("crate.key")) {
-                        $sender->sendMessage("§cYou are not allow to do that.");
+                        $sender->sendMessage($prefix . " §cYou are not allow to do that.");
                         return false;
                     }
                     if (count($args) < 2) {
@@ -181,18 +189,18 @@ class getkey extends PluginCommand{
                         $player = $this->getPlugin()->getServer()->getPlayer($args[1]);
                     if (!$player instanceof Player) {
                         if ($player instanceof ConsoleCommandSender) {
-                            $sender->sendMessage("§cPlease enter a player name.");
+                            $sender->sendMessage($prefix . " §cPlease enter a player name.");
                             return false;
                         }
-                        $sender->sendMessage("§cThat Player doesnt exit.");
+                        $sender->sendMessage($prefix . " §cThat Player doesnt exit.");
                         return false;
                     }
             $e->setLevel(-1);
             $legendaryname->addEnchantment($e);
             $legendaryname->setCustomName("§9Legendary");
             $player->getInventory()->addItem($legendaryname);
-            $player->sendMessage("§eYou receive a §9Legendary §eKey.");
-            $sender->sendMessage("§9" . $player->getName() . " §ehas received a §9Legendary §eKey.");
+            $player->sendMessage($prefix . " §eYou receive a §9Legendary §eKey.");
+            $sender->sendMessage($prefix . " §9" . $player->getName() . " §ehas received a §9Legendary §eKey.");
             break;
             default:
             $sender->sendMessage("§b===>§eKeys§b<===");
